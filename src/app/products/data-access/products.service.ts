bd7d9a493cb1d3f51a783c4bbf993b8a1ba3bbc6
Luis <luisaranaga13@gmail.com>
2025-01-15 09:48:26 -0400

    show products

--- src/app/products/data-access/products.service.ts
+++ src/app/products/data-access/products.service.ts
@@ -1,5 +1,4 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { BaseHttpService } from "../../shared/data-access/base-http.service";
 import { Product } from "../../shared/interfaces/product.interface";
 import { Observable } from "rxjs";
